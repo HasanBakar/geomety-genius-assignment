ec3634116a257b68e6f9b7b20cdeca14eeea08d8
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-02-18 06:32:53 +0600

    triangle and rectangle cart dom manipulation and validation completed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 let serial = 0;
 const pi = 3.14;
+
 function getInnerTextWithId(textFieldId) {
     const textFieldValue =document.getElementById(textFieldId).innerText;
     return textFieldValue;
 }
+function getInputFieldValueWithId(textFieldId) {
+    const inputFieldValue =document.getElementById(textFieldId).value;
+    return inputFieldValue;
+}
 
-function showTheCalculation(serial, name, area, convert) {
+function showTheCalculation(serial, name, area) {
     const tableContainer = document.getElementById('table_body_container');
     const tr = document.createElement("tr");
     tr.innerHTML =`
@@ -15,6 +20,56 @@ function showTheCalculation(serial, name, area, convert) {
     `;
 tableContainer.appendChild(tr);
 }
+
+document.getElementById('btn_triangle_calculate').addEventListener('click', function () {
+    serial++;
+    const triangle = getInnerTextWithId('title_one_triangle');
+    const baseTriangle = Number(getInputFieldValueWithId('base_triangle'));
+    const heightTriangle = Number(getInputFieldValueWithId('height_triangle'));
+    if(baseTriangle > 0 && heightTriangle > 0){
+        const area = (0.5 * baseTriangle * heightTriangle).toFixed(2);
+        showTheCalculation(serial, triangle, area);
+    }
+    else{
+  const toast =  document.getElementById('toast_message');
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="toast toast-top toast-center w-[300px]">
+  <div class="alert alert-error">
+    <div>
+      <span>Sorry! you give invalid input!!</span>
+    </div>
+  </div>
+</div>
+  `;
+  toast.appendChild(div);
+    }
+});
+
+document.getElementById('btn_rectangle').addEventListener('click', function () {
+    serial++;
+    const rectangle = getInnerTextWithId('title_two_rectangle');
+    const baseRectangle = Number(getInputFieldValueWithId('base_rectangle'));
+    const heightRectangle = Number(getInputFieldValueWithId('height_rectangle'));
+    if(baseRectangle > 0 && heightRectangle > 0){
+        const area = (baseRectangle * heightRectangle).toFixed(2);
+        showTheCalculation(serial, rectangle, area);
+    }
+    else{
+  const toast =  document.getElementById('toast_message');
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="toast toast-top toast-center w-[300px]">
+  <div class="alert alert-error">
+    <div>
+      <span>Sorry! you give invalid input!!</span>
+    </div>
+  </div>
+</div>
+  `;
+  toast.appendChild(div);
+    }
+});
 
 
 document.getElementById('btn_three').addEventListener('click', function () {
@@ -33,8 +88,8 @@ document.getElementById('btn-four').addEventListener('click', function () {
     const rhombusValueOne = parseInt(getInnerTextWithId('rhombus_value__one'));
     const rhombusValueTwo = parseInt(getInnerTextWithId('rhombus_value__two'));
     const area = 0.5 * rhombusValueOne * rhombusValueTwo;
-    const convert = 0;
-    showTheCalculation(serial, rhombus, area, convert);
+    
+    showTheCalculation(serial, rhombus, area);
 });
 
 document.getElementById('btn-five').addEventListener('click', function () {
@@ -42,9 +97,9 @@ document.getElementById('btn-five').addEventListener('click', function () {
     const pentagon = getInnerTextWithId('title_five_pentagon');
     const pentagonValueOne = parseInt(getInnerTextWithId('pentagon_value_one'));
     const pentagonValueTwo = parseInt(getInnerTextWithId('pentagon_value_two'));
-    const convert = 0; 
+    
     const area = (0.5 * pentagonValueOne * pentagonValueTwo).toFixed(2)
-    showTheCalculation(serial, pentagon, area, convert);
+    showTheCalculation(serial, pentagon, area);
 });
 
 
@@ -54,7 +109,7 @@ document.getElementById('cart-six').addEventListener('click', function () {
     const ellipseTwo = parseInt(getInnerTextWithId('ellipse__value_two'));
     const ellipse = getInnerTextWithId('title_six_ellipse');
     const area = (pi * ellipseOne * ellipseTwo).toFixed(2);
-    const convert = 0;
-    showTheCalculation(serial, ellipse, area, convert);
+    
+    showTheCalculation(serial, ellipse, area);
     
 });
